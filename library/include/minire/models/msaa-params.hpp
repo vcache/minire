0c272e8001f27b65331501aa8e37d6d2deb7b688
Igor Bereznyak <igor.bereznyak@gmail.com>
2025-09-13 19:38:48 +0300

    fix default parameter (disable AA by default)

--- library/include/minire/models/msaa-params.hpp
+++ library/include/minire/models/msaa-params.hpp
@@ -7,6 +7,6 @@ namespace minire::models
     struct MsaaParams
     {
         size_t _buffers = 0;    // SDL_GL_MULTISAMPLEBUFFERS
-        size_t _samples = 1;    // SDL_GL_MULTISAMPLESAMPLES
+        size_t _samples = 0;    // SDL_GL_MULTISAMPLESAMPLES
     };
 }
