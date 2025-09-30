a74ad7352046c516cfc05552270b2d51c5fe62bf
Igor Bereznyak <igor.bereznyak@gmail.com>
2025-09-30 11:02:16 +0300

    disable MSAA for 2D sprites (mostly for pixel texts)

--- library/sources/rasterizer.cpp
+++ library/sources/rasterizer.cpp
@@ -62,8 +62,9 @@ namespace minire
     void Rasterizer::draw3d(Scene const & scene)
     {
         // setup state for 3d mode
-        MINIRE_GL(glEnable, GL_CULL_FACE)
-        MINIRE_GL(glEnable, GL_DEPTH_TEST); 
+        MINIRE_GL(glEnable, GL_CULL_FACE);
+        MINIRE_GL(glEnable, GL_DEPTH_TEST);
+        MINIRE_GL(glEnable, GL_MULTISAMPLE);
         MINIRE_GL(glDepthFunc, GL_LESS);
         //MINIRE_GL(glCullFace, GL_FRONT);
         MINIRE_GL(glDisable, GL_BLEND);
@@ -84,6 +85,7 @@ namespace minire
         // disable depth test and blending
         MINIRE_GL(glDisable, GL_DEPTH_TEST);
         MINIRE_GL(glDisable, GL_CULL_FACE);
+        MINIRE_GL(glDisable, GL_MULTISAMPLE);
         MINIRE_GL(glEnable, GL_BLEND);
         MINIRE_GL(glBlendFunc, GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
